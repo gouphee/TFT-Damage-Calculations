988c77309d066da1be7619d1e3dcc63251311cdc
gouphee <gouphee@gmail.com>
2023-12-19 15:38:08 -0600

    done

---
+++ items.py
@@ -0,0 +1,12 @@
+items = {
+    "warmog": {
+        "HP": 600,
+        "Armor": 0,
+        "MR": 0,
+    },
+    "steadfast": {
+        "HP": 250,
+        "Armor": 20,
+        "MR": 0,
+    }
+}
---
+++ main.py
@@ -0,0 +1,111 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+
+def damage_calculation(hp, armor, mr):
+
+    warmogPhy = (hp + 600)*1.08 * ((100+armor)/100)
+    warmogMag = (hp + 600)*1.08 * ((100+mr)/100)
+
+    warmog = (hp + 600)*1.08 * ((100+mr)/100)
+
+    steadfastPhy = ((hp + 250) / 2)  * (100+armor+20)/100 * (1/.85) + ((hp + 250) / 2)  * (100+armor+20)/100 * (1/.92)
+    steadfastMag = ((hp + 250) / 2)  * (100+mr)/100 * (1/.85) + ((hp + 250) / 2)  * (100+mr)/100 * (1/.92)
+
+    return [warmog, steadfastPhy, steadfastMag]
+
+def resistance_graph():
+    resistance = [[],[],[], range(201)]
+    for i in range(201):
+        for index, val in enumerate(damage_calculation(1800, i, i)):
+            resistance[index].append(val)
+    df = pd.DataFrame(data=resistance)
+    df = df.transpose()
+    df.columns = ['warmog', 'steadfastPhy', 'steadfastMag', 'Resistance']
+    df['PhyRatio'] = (df['steadfastPhy']/df['warmog'])
+    df['MagRatio'] = (df['steadfastMag']/df['warmog'])
+    df['PhyDiff'] = (df['steadfastPhy'] - df['warmog'])
+    df['MagDiff'] = (df['steadfastMag'] - df['warmog'])
+    diff = df[['Resistance', 'PhyDiff', 'MagDiff']]
+
+    sns.set_theme(style="darkgrid")
+
+    g = sns.lineplot(data=pd.melt(diff, ['Resistance']), x='Resistance', y="value", hue='variable')
+
+    g.set(ylabel='Effective HP Difference (Steadfast - Warmog)')
+
+    plt.show()
+
+def hp_graph():
+    hp = [[],[],[], range(10001)]
+    for i in range(10001):
+        for index, val in enumerate(damage_calculation(i, 50, 50)):
+            hp[index].append(val)
+    df = pd.DataFrame(data=hp)
+    df = df.transpose()
+    df.columns = ['warmog', 'steadfastPhy', 'steadfastMag', 'HP']
+    df['PhyRatio'] = (df['steadfastPhy']/df['warmog'])
+    df['MagRatio'] = (df['steadfastMag']/df['warmog'])
+    df['PhyDiff'] = (df['steadfastPhy'] - df['warmog'])
+    df['MagDiff'] = (df['steadfastMag'] - df['warmog'])
+    diff = df[['HP', 'PhyDiff', 'MagDiff']]
+
+    sns.set_theme(style="darkgrid")
+
+    g = sns.lineplot(data=pd.melt(diff, ['HP']), x='HP', y="value", hue='variable')
+
+    g.set(ylabel='Effective HP Difference (Steadfast - Warmog)')
+
+    plt.show()
+
+def surface_plot():
+    arrayP = []
+    arrayM = []
+    for i in range(10001):
+        resistP = []
+        resistM = []
+        for j in range(201):
+            vals = damage_calculation(i, j, j)
+            resistP.append(vals[1]-vals[0])
+            resistM.append(vals[2]-vals[0])
+        arrayP.append(resistP)
+        arrayM.append(resistM)
+    dfP = pd.DataFrame(arrayP)
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Create an X-Y mesh of the shape of the dataframe
+    X, Y = np.meshgrid(range(len(dfP.columns)), range(len(dfP.index)))
+    
+    # Convert the dataframe to a 2D array for plotting
+    Z = dfP.values
+
+    z_min, z_max = Z.min(), Z.max()
+    z_range = z_max - z_min
+    ax.set_zlim(-z_max - z_range * 0.1, z_max + z_range * 0.1)
+
+    # Plot the surface
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, vmin=-z_max, vmax=z_max, linewidth=0)
+
+    ax.set_xlabel("Resistances")
+    ax.set_ylabel("HP")
+    ax.set_zlabel("Effective HP Difference (Steadfast - Warmog)")
+
+    # Add a color bar which maps values to colors
+    fig.colorbar(surf, shrink=0.5, aspect=10, pad=0.1)
+
+    plt.show()
+
+
+def main():
+    # resistance_graph()
+    # hp_graph()
+    surface_plot()
+    
+
+
+if __name__ == "__main__":
+    main()
